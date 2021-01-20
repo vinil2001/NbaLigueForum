12fac78bad84a1cc621fac8fb872e77606b6b89e
vinil2001@ukr.net <vinil2001@ukr.net>
2021-01-20 12:07:48 +0200

    IForum

---
+++ NbaLigueForum.Data/IForum.cs
@@ -0,0 +1,23 @@
+﻿using ForumCore.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumCore.Data
+{
+    public interface IForum
+    {
+        Forum GetById(int id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateForumTitle(int forumId, string newForumTitle);
+        Task UpdateForumDescription(int forumId, string newForumDescription);
+
+
+    }
+}
+
